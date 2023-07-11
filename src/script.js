d8f5c7b9217fdf5d663e0a873f323bb437c77236
sum.buicong <sum.buicong@halliburton.com>
2023-07-11 10:30:27 +0700

    Adding some objects to scene

--- src/script.js
+++ src/script.js
@@ -10,17 +10,85 @@ import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from 'three';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GUI } from 'dat.gui'
+import { buildRingGeometry } from './utils'
 
-//================================================ SCENE ====================================================
+// ================================================ SCENE ====================================================
 const scene = new Scene();
 
-//================================================ OBJECTS ==================================================
-const geometry = new THREE.SphereGeometry(5, 16, 32);
-const material = new THREE.MeshBasicMaterial({ color: new THREE.Color(0xFF0000) });
+// ================================================ OBJECTS ==================================================
+const BaseWellString = {
+  trajectoryX: [1.15, 1.05, 0, 0],
+  trajectoryY: [0.25, 0.25, 0, 0],
+  tvd: [-5, 0, 5, 10],
+};
+let vectors = [];
+const trajectoryX = BaseWellString.trajectoryX;
+const trajectoryY = BaseWellString.trajectoryY;
+const tvd = BaseWellString.tvd;
+const minLength = Math.min(BaseWellString.trajectoryX.length, BaseWellString.trajectoryY.length, BaseWellString.tvd.length);
+for (let index = 0; index < minLength; index++) {
+  vectors.push(new THREE.Vector3(trajectoryX[index], trajectoryY[index], tvd[index]));
+}
+const lastPoint = vectors[vectors.length - 1];
+const path = new THREE.CatmullRomCurve3(vectors);
+
+// Drawing line
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(vectors);
+const lineMaterial = new THREE.LineBasicMaterial({
+  color: 0xffff00
+});
+const line = new THREE.Line(lineGeometry, lineMaterial);
+scene.add(line);
+
+// Drawing tube
+const outerRadius = 0.5;
+const innerRadius = 0.4;
+
+const geometry = buildRingGeometry(outerRadius, innerRadius, path);
+geometry.computeVertexNormals();
+geometry.computeBoundingBox();
+geometry.computeBoundingSphere();
+let { min, max } = geometry.boundingBox;
+
+let offset = new THREE.Vector2(0 - min.x, 0 - min.y);
+let range = new THREE.Vector2(max.x - min.x, max.y - min.y);
+console.log(offset, range)
+
+const position = geometry.attributes.position;
+const uvs = [];
+for (let i = 0; i < position.count; i++) {
+  const v3 = new Vector3().fromBufferAttribute(position, i);
+  uvs.push((v3.x + offset.x) / range.x);
+  uvs.push((v3.y + offset.y) / range.y);
+}
+
+geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvs), 2));
+geometry.setAttribute('uv2', new THREE.BufferAttribute(new Float32Array(uvs), 2));
+geometry.attributes.uv.needsUpdate = true;
+geometry.attributes.uv2.needsUpdate = true;
+// const geometry = new THREE.TubeGeometry(path, tubularSegemets, outerRadius, 8, false);
+// const geometry = new THREE.PlaneGeometry(8, 10, 32, 16);
+const texture = new THREE.TextureLoader().load('bha-node-rotating-effect.png');
+texture.wrapT = texture.wrapS = THREE.RepeatWrapping;
+texture.repeat.set(1, 1)
+
+const material = new THREE.MeshLambertMaterial({
+  color: new THREE.Color(0xFF0000),
+  map: texture,
+  depthWrite: true,
+  opacity: 1,
+  side: THREE.DoubleSide,
+  transparent: false,
+  // wireframe: true
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-//================================================ CAMERA ===================================================
+// ================================================ AxesHelper ===============================================
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// ================================================ CAMERA ===================================================
 const camera = new PerspectiveCamera();
 camera.position.set(0, 10, 15);
 camera.lookAt(new Vector3(0, 0, 0));
@@ -42,6 +110,9 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // =============================================== RENDER LOOP ==============================================
 const onAnimationFrameHandler = (timeStamp) => {
   renderer.render(scene, camera);
+  if (texture) {
+    texture.offset.x += 0.005;
+  }
   controls.update();
   window.requestAnimationFrame(onAnimationFrameHandler);
 }
---
+++ src/utils.js
@@ -0,0 +1,80 @@
+import * as THREE from 'three'
+
+export function rotateAroundObjectAxis(object, axis, radians) {
+  const matrix = new THREE.Matrix4();
+  matrix.makeRotationAxis(axis.normalize(), radians);
+
+  // old code for Three.JS pre r54:
+  // object.matrix.multiplySelf(rotObjectMatrix);      // post-multiply
+  // new code for Three.JS r55+:
+  object.matrix.multiply(matrix);
+
+  // old code for Three.js pre r49:
+  // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+  // old code for Three.js r50-r58:
+  // object.rotation.setEulerFromRotationMatrix(object.matrix);
+  // new code for Three.js r59+:
+  object.rotation.setFromRotationMatrix(object.matrix);
+}
+
+// Rotate an object around an arbitrary axis in world space
+export function rotateAroundWorldAxis(object, axis, radians) {
+  const matrix = new THREE.Matrix4();
+  matrix.makeRotationAxis(axis.normalize(), radians);
+
+  // old code for Three.JS pre r54:
+  //  rotationMatrix.multiply(object.matrix);
+  // new code for Three.JS r55+:
+  matrix.multiply(object.matrix);                // pre-multiply
+
+  object.matrix = matrix;
+
+  // old code for Three.js pre r49:
+  // object.rotation.getRotationFromMatrix(object.matrix, object.scale);
+  // old code for Three.js pre r59:
+  // object.rotation.setEulerFromRotationMatrix(object.matrix);
+  // code for r59+:
+  object.rotation.setFromRotationMatrix(object.matrix);
+}
+
+export function buildRingGeometry(outerRadius, innerRadius, path) {
+  const shape = new THREE.Shape();
+  const sAngle = THREE.MathUtils.degToRad(-180);
+  const eAngle = THREE.MathUtils.degToRad(180);
+  shape.absarc(0, 0, outerRadius, sAngle, eAngle, false);
+  shape.absarc(0, 0, innerRadius, eAngle, sAngle, true);
+
+  const tubularSegments = Math.round(path.getLength());
+  return new THREE.ExtrudeGeometry(shape, {
+    curveSegments: 4,
+    extrudePath: path,
+    steps: tubularSegments
+  })
+}
+
+export  function buildSphere(radius) {
+  const geometry = new THREE.SphereGeometry(radius, 32, 16);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  return new THREE.Mesh(geometry, material);
+}
+
+export function toMesh(mesh) {
+  const position = mesh.geometry.attributes.position;
+  const vector = new THREE.Vector3();
+  const division = 1 / 100;
+  const length = Math.round(position.array.length / 8)
+  for (let i = 0; i < length; i = i + 2) {
+    // vector.fromBufferAttribute(position, i);
+    // vector.applyMatrix4(mesh.matrixWorld);
+
+    const updateX = vector.x * Math.cos(division) - vector.y * Math.sin(division);
+    const updateY = vector.y * Math.cos(division) + vector.x * Math.sin(division);
+    mesh.geometry.attributes.position.setX(i, updateX);
+    mesh.geometry.attributes.position.setY(i, updateY);
+    mesh.geometry.dispose();
+    // const _sphere = sphere.clone();
+    // _sphere.position.set(vector.x, vector.y, vector.z);
+    // scene.add(_sphere);
+    // if (i === 0) break
+  }
+}
